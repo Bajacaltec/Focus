512ad38a0c1cf24e40f01f8dd21a121c0695b44d
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-03-26 16:40:12 -0600

    a

--- app.py
+++ app.py
@@ -11,11 +11,6 @@ st.set_page_config(layout='wide',page_title='Haz tu tesis')
 #iconos poner el nombre de bootstrap
 # with st.sidebar: (ponerlo en el sidebar)
 
-cur=sqlite3.connect('Db.db')
-con=cur.cursor()
-con.execute('''CREATE TABLE IF NOT EXISTS proteus (Folio INTEGER PRIMARY KEY,
-            Institucion TEXT)''')
-cur.commit()
 
 
 seleccion=option_menu(None,['Portada','Índice','Just','Hipo','Plant','M-T'],orientation='horizontal',icons=['journal-medical','card-list','type'])
@@ -26,16 +21,7 @@ if seleccion=='Portada':
         institucion,titulo,autores=st.columns([1,2,1])
         with institucion:
             x=st.text_input('Institución:')
-            con.execute('INSERT INTO proteus VALUES(1,"hola")')
-            cur.commit()
-        with titulo:
-            st.text_area('Título:')
-
-        with autores:
-            st.text_input('Autor 1:')
-            st.text_input('Autor 2:')
-            st.text_input('Autor 3:')
-            
+    
             
 
 
