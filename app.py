54ccab8e8f107daf9663df05f8f86e0ea4f995ca
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-03-26 10:29:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
+from boto import config
+from matplotlib.ft2font import HORIZONTAL, VERTICAL
+from matplotlib.pyplot import title
 import streamlit as st
 from streamlit_option_menu import option_menu
-
+st.set_page_config(layout='wide',page_title='Haz tu tesis',page_icon='🦈')
 # revisar este como nicial
-st.title('Haz tu tésis')
 #Menu#
+# modificación del menú#
+#iconos poner el nombre de bootstrap
+# with st.sidebar: (ponerlo en el sidebar)
+seleccion=option_menu(None,['Portada','Índice','Just','Hipo','Plant','M-T'],orientation='horizontal',icons=['journal-medical','card-list','type'])
 
-with st.sidebar:
-    seleccion=option
+#Portada
+if seleccion=='Portada':
+        st.write('Menú')
+        institucion,titulo,autores=st.columns([1,2,1])
+        with institucion:
+            st.text_input('Institución:')
+        with titulo:
+            st.text_area('Título:')
+
+        with autores:
+            st.text_input('Autor 1:')
+            st.text_input('Autor 2:')
+            st.text_input('Autor 3:')
 
 
 
 
+#deploy la app en Render
+#https://www.youtube.com/watch?v=4SO3CUWPYf0
 
+#subir app de streamlit en linea a Render
+#https://www.youtube.com/watch?v=4SO3CUWPYf0
 
-
-
-
+#hacer un cv en linea
+#https://www.youtube.com/watch?v=BXAeMICmUSQ
 
 
 #https://www.youtube.com/watch?v=3egaMfE9388
@@ -23,7 +43,7 @@ with st.sidebar:
 #https://www.youtube.com/watch?v=VqgUkExPvLY
 
 #Navigation MEnu
-https://youtu.be/hEPoto5xp3k
+#https://youtu.be/hEPoto5xp3k
 
 # hacer uso de la autenticatión app de streamlit para controlar accesos
 # https://github.com/mkhorasani/Streamlit-Authenticator
@@ -33,6 +53,5 @@ https://youtu.be/hEPoto5xp3k
 #Baja Caltec o Nunztec
 #Nombre de la webapp
 
-menu=st.selectbox('Menú',['Portada','Índice'])
 
 
