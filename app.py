d5f1831070855f8995cce7f582b4015e2fa1546c
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-03-26 00:27:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
+# revisar este como nicial
+st.title('Haz tu tésis')
+#Menu#
+
+with st.sidebar:
+    seleccion=option
+
+
+
+
+
+
+
+
+
+
+
+#https://www.youtube.com/watch?v=3egaMfE9388
+
+#https://www.youtube.com/watch?v=VqgUkExPvLY
+
+#Navigation MEnu
+https://youtu.be/hEPoto5xp3k
 
 # hacer uso de la autenticatión app de streamlit para controlar accesos
 # https://github.com/mkhorasani/Streamlit-Authenticator
@@ -11,4 +35,4 @@ import streamlit as st
 
 menu=st.selectbox('Menú',['Portada','Índice'])
 
-st.text_input('Ponle')
+
