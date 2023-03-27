2a915ac84314947ed79cb370e97d5e47be4970da
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-03-26 18:55:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(layout='wide',page_title='Haz tu tesis')
 # modificación del menú#
 #iconos poner el nombre de bootstrap
 # with st.sidebar: (ponerlo en el sidebar)
-
+st.write('Prueba de upload')
 #Base render
 conn = psycopg2.connect("dbname=base_vhom user=base_vhom_user password=8xUvM1YA3iw7Rjb1FxPTkgRf2xEFl87T host=dpg-cggclf02qv28tc396eng-a Port=5432")
 
