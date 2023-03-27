9b81868bb2e5a5fd359e8d0a76f8205f06b0d2bf
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-03-26 19:54:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ import streamlit as st
 import psycopg2
 from streamlit_option_menu import option_menu
 st.set_page_config(layout='wide',page_title='Haz tu tesis')
+
+seleccion=option_menu(None,['Portada','Índice','Just','Hipo','Plant','M-T'],orientation='horizontal',icons=['journal-medical','card-list','type'])
+#Portada
+
+st.write('Menú')
+institucion,titulo,autores=st.columns([1,2,1])
+with institucion:
+    id=st.number_input('id)')
+    x=st.text_input('Institución:')
 #Menu#
 # modificación el menú#
 #iconos poner el nombre de bootstrap
@@ -12,67 +21,22 @@ st.set_page_config(layout='wide',page_title='Haz tu tesis')
 st.write('Prueba de upload')
 #Base render
 conn = psycopg2.connect("dbname=base_vhom user=base_vhom_user password=8xUvM1YA3iw7Rjb1FxPTkgRf2xEFl87T host=dpg-cggclf02qv28tc396eng-a port=5432")
+cur=conn.cursor()
 
-def create_tables():
-    """ create tables in the PostgreSQL database"""
-    commands = (
-        """
-        CREATE TABLE vendors (
-            vendor_id SERIAL PRIMARY KEY,
-            vendor_name VARCHAR(255) NOT NULL
-        )
-        """,
-        """ CREATE TABLE parts (
-                part_id SERIAL PRIMARY KEY,
-                part_name VARCHAR(255) NOT NULL
-                )
-        """,
-        """
-        CREATE TABLE part_drawings (
-                part_id INTEGER PRIMARY KEY,
-                file_extension VARCHAR(5) NOT NULL,
-                drawing_data BYTEA NOT NULL,
-                FOREIGN KEY (part_id)
-                REFERENCES parts (part_id)
-                ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        """,
-        """
-        CREATE TABLE vendor_parts (
-                vendor_id INTEGER NOT NULL,
-                part_id INTEGER NOT NULL,
-                PRIMARY KEY (vendor_id , part_id),
-                FOREIGN KEY (vendor_id)
-                    REFERENCES vendors (vendor_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE,
-                FOREIGN KEY (part_id)
-                    REFERENCES parts (part_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        """)
-    conn = None
-    try:
-        # read the connection parameters
-        params = config()
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        # create table one by one
-        for command in commands:
-            cur.execute(command)
-        # close communication with the PostgreSQL database server
-        cur.close()
-        # commit the changes
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+creartabla='''CREATE TABLE IF NOT EXISTS prueba (id int PRIMARY KEY,
+institucion varchar(40)'''
+
+insertartabla='''INSERT INTO prueba(id,institucion) VALUES(%s,%s)'''
+insertarvalores=(id,x)
+cur.execute(insertartabla,insertarvalores)
+
+cur.execute(creartabla)
+conn.commit()
 
 
-if __name__ == '__main__':
-    create_tables()
+cur.close()
+conn.close()
+
 seleccion=option_menu(None,['Portada','Índice','Just','Hipo','Plant','M-T'],orientation='horizontal',icons=['journal-medical','card-list','type'])
 #Portada
 if seleccion=='Portada':
