6360e7962150861039df5893e8391fafe425b454
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-03-28 21:46:19 -0600

    a

--- app.py
+++ app.py
@@ -2,27 +2,53 @@
 from matplotlib.ft2font import HORIZONTAL, VERTICAL
 from matplotlib.pyplot import title
 import streamlit as st
+import requests
+import json
+from streamlit_lottie import st_lottie
 import psycopg2
 from streamlit_option_menu import option_menu
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
 st.set_page_config(layout='wide',page_title='Haz tu tesis')
-seleccion=option_menu(None,['Principal','Portada','Índice','Just','Hipo','Plant','M-T'],orientation='horizontal',icons=['journal-medical','card-list','type'])
+
+eins,swei,drei=st.columns([1,2,1])
+with eins:
+    st.write('lotbctec')
+with drei:
+    mex=load_lottieurl('https://assets6.lottiefiles.com/packages/lf20_jidselhy.json')
+    st_lottie(mex,height=50)
+seleccion=option_menu(None,['Principal','Asesores','Trabajos publicados','Contacto','Ingresa'],orientation='horizontal',icons=['journal-medical','card-list','type'])
 
 if seleccion=='Principal':
     uno,dos,tres=st.columns([1,2,1])
     with uno:
+        lotbook=load_lottieurl('https://assets1.lottiefiles.com/packages/lf20_tnrzlN.json')
+        st_lottie(lotbook)
         st.info('En Tesisya nos encargamos de guiarte con nuestra Webapp, personalizada para el trabajo de investigación en cuestión te apoyamos desde el inicio en cuanto a la elección de tema de investigación, búsqueda de información, desarrollo del proyecto, análisis estadístico CORRECTO, escritura de la tesis, publicación y traducción al ingles')
+        
     with dos:
+        lottie_hello = load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_iYvSqSMKZB.json")
+            
         st.subheader('¿No sabes por donde empezar?')
         st.info('¿Cual es tu tema de interes?')
         st.info('Documentate, investiga la información actual')
         st.info('¿Cual es la pregunta de investigación')
+        st_lottie(lottie_hello,height=150)
+
     with tres:
+
         st.image('/Users/alonso/Focus/Imagenes/tesiscxcolcardio.png')
         st.info('Realiza tu proyecto de investigación en la modalidad y alcance que requieras en tiempo record')
 
     
-elif seleccion=='Portada':
-        
+elif seleccion=='Ingresa':
+    seleccion1=option_menu(None,['Principal','Portada','Índice','Just','Hipo','Plant','M-T'],orientation='horizontal',icons=['journal-medical','card-list','type'])
+
     #Portada
     institucion,titulo,autores=st.columns([1,2,1])
     with institucion:
