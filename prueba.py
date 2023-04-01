159c25e8830e605a1d9da644e5a49f918b653b36
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-04-01 15:42:52 -0600

    a

---
+++ prueba.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('Bienvenidos a TESSextractor')
+
+st.info('Utilizamos el lenguaje ')
+st.success('Python')
+st.info('R')
+
+st.subheader('Con esto podemos realizar un trabajo de mejor calidad en el desarrolo de tu tesis de investigación y análisis estadístico')
+
+st.write('Aquí puedes poner tus datos relacionados con tu investigación')
+
+st.text_area('Tus datos')
+
+st.info('Nosotros analizaremos tus datos y te entregaremos el análisis y resultado de tu tesis')
