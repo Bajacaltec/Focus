147d01c15f0c430c027aab7f03bb2a755f4f87d5
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-04-01 21:46:08 -0600

    a

--- dinero.py
+++ dinero.py
@@ -2,18 +2,38 @@ from email.mime import image
 from importlib.resources import path
 import streamlit as st
 from pathlib import Path
-
+import requests
+from streamlit_lottie import st_lottie
+def load_lottieurl(url: str):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
 
 def money():
-    st.title('Elige el servicio que necesitas')
-   
-    #generales
-    strype_salida= "https://buy.stripe.com/14k2aQ9GJfDI8mYaEE"
-    email="mdfnunez@gmail.com"
+    st.subheader('Pago')
+    st.caption('Elige el servicio que necesitas para tu proyecto')
+    st.write("")
+    st.empty()
+    st.write("")
     
+
+    #generales
+    st.info('Antes de realizar alguna transferencia, contactanos para tener seguimiento por parte de un editor')
+    st.info('El editor resolvera vía email todas tus dudas y los pormenores de los servicios, tiempos de entrega, etc.')
+
+    sol1,sol2=st.columns([2,1])
+    with sol1:
+        st.empty()
+    with sol2:
+
+        dinero=load_lottieurl('https://assets10.lottiefiles.com/packages/lf20_djwnoxew.json')
+        st_lottie(dinero,height=150)
+        
+        
     colinit,colizq,colcentr,colder=st.columns(4)
     with colinit:
-        st.markdown("[![Foo](http://www.google.com.au/images/nav_logo7.png)](https://buy.stripe.com/14k2aQ9GJfDI8mYaEE)")
+        st.markdown("[![Foo](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iedunote.com%2Fresearch-process&psig=AOvVaw26ZZwNF41_ujerCpVf2yV_&ust=1680493391500000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJDXjO-jiv4CFQAAAAAdAAAAABAE)](https://buy.stripe.com/14k2aQ9GJfDI8mYaEE)")
         st.caption('Protocolo de investigación nivel licenciatura')
     with colizq:
         #clickable image
@@ -26,5 +46,7 @@ def money():
 
 
     with colder:
-        st.markdown("[![Foo](http://www.google.com.au/images/nav_logo7.png)](https://buy.stripe.com/14k2aQ9GJfDI8mYaEE)")
+        st.markdown("[![Foo](/Users/alonso/Focus/Imagenes/Webapp medpost.png)](https://buy.stripe.com/14k2aQ9GJfDI8mYaEE)")
+        st.caption('Poster para congreso')
+
 
