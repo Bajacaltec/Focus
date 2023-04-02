aa2ad3727cb3c4f49611696100f9a0921e50c3de
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-04-02 14:19:07 -0600

    a

--- dinero.py
+++ dinero.py
@@ -11,12 +11,6 @@ def load_lottieurl(url: str):
         return r.json()
 
 def money():
-    st.subheader('Pago')
-    st.caption('Elige el servicio que necesitas para tu proyecto')
-    st.write("")
-    st.empty()
-    st.write("")
-    
     x="""<head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
@@ -26,13 +20,14 @@ def money():
             height: 50px;
             width: 100px;
             margin-top: 20px;
+            style= background-color:#2BE039
         }
     </style>
 </head>
 <body>
     <div class="container">
-        <h1 style="text-align:center;"></h1>
-        <a href="https://checkout.stripe.com/c/pay/cs_live_a15DxBP3qQObEUvZguNuMa6lpP18qvUoYQOKxe849qmAiwXWDQ0crJO7q9#fidkdWxOYHwnPyd1blppbHNgWjA0SHdfdDJDXH02VjJ8R11ESXdENDMzTk5hNX9oSUFHc2t1VGJBMkh%2FQ2ROUm8wcGJJSHFRcDJ2cTZof1ddQlFsVjdNfDR1M3Q0TTVhN0IwQXNrfEJ2UUZvNTVQcUF%2FVEhAUicpJ3VpbGtuQH11anZgYUxhJz8nNDFuN2RUPEJPY0FMPWhcZEBAJ3gl">
+        <h1    "text-align:center;"></h1>
+        <a href="https://buy.stripe.com/fZeaHm9GJfDIeLmbIJ">
             <button class="btn btn-primary btn-lg">Comprar</button>
         </a>
     </div>
@@ -40,9 +35,41 @@ def money():
     
 
 """
-    dinero=load_lottieurl('https://assets10.lottiefiles.com/packages/lf20_djwnoxew.json')
-    st_lottie(dinero,height=150)
-    st.caption('Antes de realizar alguna transferencia, contactanos para tener seguimiento por parte de un editor')
+    gol1,gol2=st.columns([5,2])
+    with gol1:
+        st.subheader('Área de pago')
+        st.caption('Se aceptan todas las tarjetas American express, Visa o Mastercard, depósito en OXXO y apple Pay')
+        st.write('Antes de realizar alguna transferencia, contactanos para tener seguimiento por parte de un editor')
+    with gol2:
+        dinero=load_lottieurl('https://assets10.lottiefiles.com/packages/lf20_djwnoxew.json')
+        st_lottie(dinero,height=150)
+    st.markdown('__________')
+
+    lol2,lol3,lol4=st.columns([3,1,1])
+    
+    with lol2:
+        st.caption('Producto')
+        st.subheader('Análisis estadístico')
+        st.caption('Análisis con R, el cliente debe proporcionar base de datos completa')
+
+    with lol3:
+        st.image('/Users/alonso/Focus/Imagenes/anestadistica.jpg')
+
+    with lol4:
+        st.markdown(x,unsafe_allow_html=True)
+        st.subheader("")
+    st.markdown('_________')
+
+
+        
+
+        
+
+
+    
+    
+    
+    
 
     
 
@@ -50,9 +77,7 @@ def money():
         
         
     colinit,colizq,colcentr,colder=st.columns(4)
-    with colinit:
-        st.image('Imagenes/Webapp medpost.png')
-        st.markdown(x,unsafe_allow_html=True)
+    
 
     with colizq:
         #clickable image
